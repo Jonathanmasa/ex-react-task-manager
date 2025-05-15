6d929b86b454ed06a1c55b9fb5aa6aa7f0bdb98b
Jonathanmasa <jonathanmasa97@gmail.com>
2025-05-15 18:35:00 +0200

    Add modal confirmation for task deletion in TaskDetail component

--- src/Pages/TaskDetail.jsx
+++ src/Pages/TaskDetail.jsx
@@ -1,6 +1,7 @@
 import { useParams, useNavigate } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { GlobalContext } from "../context/GlobalContext";
+import Modal from "../Components/Modal";
 
 export default function TaskDetail() {
     const { id } = useParams();
@@ -9,6 +10,8 @@ export default function TaskDetail() {
 
     const taskId = Number(id);
     const task = tasks.find((task) => task.id === parseInt(id));
+
+    const [showModal, setShowModal] = useState(false);
 
     if (!task) {
         return <h1>Task non trovato</h1>;
@@ -35,7 +38,17 @@ export default function TaskDetail() {
             <p><strong>{task.description}</strong></p>
             <p><strong>Stato: {task.status}</strong></p>
             <p><strong>Creato il: {new Date(task.createdAt).toLocaleDateString()}</strong></p>
-            <button onClick={handleDelete}>Delete Task</button>
+            <button onClick={() => setShowModal(true)}>Delete Task</button>
+
+            
+            <Modal
+                title="Conferma eliminazione"
+                content="Sei sicuro di voler eliminare questa task?"
+                show={showModal}
+                onClose={() => setShowModal(false)}
+                onConfirm={handleDelete}
+                confirmText="Elimina"
+            />
 
         </div>
     );
