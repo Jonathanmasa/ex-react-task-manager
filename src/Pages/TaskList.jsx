99665ea87027b7ff6a2555357a0c55fcfa8296f5
Jonathanmasa <jonathanmasa97@gmail.com>
2025-05-19 10:29:57 +0200

    Implement sorting functionality in TaskList and enhance button styling in index.css

--- src/Pages/TaskList.jsx
+++ src/Pages/TaskList.jsx
@@ -1,9 +1,22 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { GlobalContext } from "../context/GlobalContext";
 import TaskRow from "../Components/TaskRow";
 
 export default function TaskList() {
     const { tasks } = useContext(GlobalContext);
+    const [sortBy, setSortBy] = useState("createdAt");
+    const [sortOrder, setSortOrder] = useState(1);
+
+    const sortIcon = sortOrder === 1 ? "↓" : "↑";
+
+    const handleSort = (field) => {
+        if (sortBy === field) {
+            setSortOrder(prev => prev * -1);
+        } else {
+            setSortBy(field);
+            setSortOrder(1);
+        }
+    }
 
     return (
             <div className="nav-content">
@@ -11,9 +24,9 @@ export default function TaskList() {
                 <table>
                     <thead>
                         <tr>
-                            <th>Nome</th>
-                            <th>Status</th>
-                            <th>Data di Creazione</th>
+                            <th onClick={()=> handleSort('title')}>Nome {sortBy === 'title' && sortIcon}</th>
+                            <th onClick={()=> handleSort('status')}>Status {sortBy === 'status' && sortIcon}</th>
+                            <th onClick={()=> handleSort('createdAt')}>Data di Creazione {sortBy === 'createdAt' && sortIcon}</th>
                         </tr>
                     </thead>
                     <tbody>
